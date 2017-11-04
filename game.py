17c2ac655ea51fd3f1eff1d2b9e52ef5709a3564
Boogie <steven.anaya@student.nmt.edu>
2017-11-03 22:33:55 -0600

    Initialize countries in main

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 #! /usr/env python3
 '''Main runtime file for game'''
 
+import country
 from init import ask_names
 from init import welcome
 
@@ -8,6 +9,10 @@ def main():
     'Main'
     welcome()
     countries = ask_names()
+    country_a = country.Computer(countries['A'])
+    country_b = country.Computer(countries['B'])
+    country_c = country.Computer(countries['C'])
+    country_d = country.Computer(countries['D'])
 
 if __name__ == '__main__':
     exit(main())
